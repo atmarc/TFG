95316ff003042434f9df19b5158b4eda64f4df19
atmarc <marc.amoros.trepat@gmail.com>
2021-03-26 18:58:01 +0100

    changes made on personal computer

--- random_walks.m
+++ random_walks.m
@@ -1,49 +1,68 @@
-global L;
-global dt;
-global Rmax;
-global N;
-global grid;
+global L; global dt; global Rmax; global N; global grid;
 
-iterations = 5000;
-n_walkers = 1000;
+iterations = 10000;
+n_walkers = 300;
+rec_lvl = 1;
 L = 1;
-N = 3^6;
-dt = L/(3^6); % L/3^i
+N = 3^5;
+dt = (L/(3^rec_lvl))^2/10; % L/3^i
 Rmax = L/2;
  
-grid = sierpinski(N, 6, true);
-data = zeros(1,iterations);
+grid = sierpinski(N, rec_lvl, false);
+
+data_range = iterations;
+distances = zeros(1, data_range);
 walkers = init_walkers(n_walkers);
 
-for i=1:n_walkers
-    if mod(i, 50) == 0
-        display(['Walker ' num2str(i)]);
-    end
+for i_rec=1:9
+    rec_lvl = 1;
     
-    for j=1:iterations
-        walkers(i) = update_walker(walkers(i));
-        dx = walkers(i).init_x - walkers(i).x;
-        dy = walkers(i).init_y - walkers(i).y;
-
-        data(j) = data(j) + dx^2 + dy^2;
-        %display([num2str(g_posx) ' ' num2str(g_posy) ' ' num2str(walkers(i).x_pbc) ' ' num2str(walkers(i).y_pbc)]);
+    for i=1:n_walkers
+        if mod(i, 50) == 0
+            display(['Walker ' num2str(i)]);
+        end
         
+        i_data = 1;
+        for j=1:iterations
+            if mod(j, 50000) == 0
+                display(['Iteration ' num2str(j)]);
+            end
+            walkers(i) = update_walker(walkers(i));
+            dx = walkers(i).init_x - walkers(i).x;
+            dy = walkers(i).init_y - walkers(i).y;
+
+            if mod(j, floor(iterations/data_range)) == 0 && i_data <= data_range
+                distances(i_data) = distances(i_data) + dx^2 + dy^2;
+                i_data = i_data + 1;
+            end
+        end
     end
+
+
+    % ------------------ PLOTS ------------------
+    % mean value
+    distances = distances / n_walkers;
+
+    x = 1:data_range;
+    x = x * dt * iterations/data_range;
+
+    p = polyfit(x, distances, 1);
+    y_pred = polyval(p, x);
+
+    disp('Ajuste:');
+    disp(['y = x*' num2str(p(1)) ' + ' num2str(p(2))]);
+
+    %plot(x, distances, x, 2*x, x, y_pred);
+
 end
 
-%imagesc(grid);
+% ----------------- METHODS -----------------
 
-% mean value
-data = data / n_walkers;
-x = 1:iterations;
-x = x * dt;
-
-p = polyfit(x, data, 1);
-y_pred = polyval(p, x);
-
-disp('Ajuste:');
-disp(['y = x*' num2str(p(1)) ' + ' num2str(p(2))]);
-plot(x, data, x, 2*x, x, y_pred);
+function r = save_to_file(filename, data)
+    fileID = fopen(filename, 'a');
+    fprintf(fileID, [data '\n']);
+    fclose(fileID);
+end  
 
 function res = init_walkers(n_walkers)
     global L; global Rmax;
@@ -70,11 +89,11 @@ end
 function res = update_walker(walker)
     tmp = get_random_pos(walker);
     
-    while ~valid_pos(tmp.x_pbc, tmp.y_pbc)
-        tmp = get_random_pos(walker);
+    if valid_pos(tmp.x_pbc, tmp.y_pbc)
+        res = tmp;
+    else
+        res = walker;
     end
-
-    res = tmp;
 end
 
 function r = valid_pos(x, y)
@@ -86,7 +105,7 @@ function r = valid_pos(x, y)
 end
 
 function r = get_random_pos(walker)
-    global dt; global Rmax; global L;
+    global dt; global Rmax;
     
     % Update movement
     [a, b] = random_values();
@@ -95,9 +114,8 @@ function r = get_random_pos(walker)
 
     walker.y = walker.y + b * sqrt(dt);
     walker.y_pbc = walker.y_pbc + b * sqrt(dt);
-
-    %disp([num2str(walker.x_pbc) ' ' num2str(walker.y_pbc)])
-    % check if it is in a valid position
+    
+    % check if it is in a valid position to preserve pbc
     if walker.x_pbc <= -Rmax
         walker.x_pbc = mod(walker.x_pbc, Rmax); 
     end
@@ -110,7 +128,7 @@ function r = get_random_pos(walker)
     if walker.y_pbc >= Rmax
         walker.y_pbc = mod(walker.x_pbc, -Rmax);
     end
-    %disp([num2str(walker.x_pbc) ' ' num2str(walker.y_pbc)])
+    
     r = walker;
 end
 
